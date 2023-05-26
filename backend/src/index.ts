6ca6ff58acb8df2fb4ea33b297293ed1a7587056
huy1999dnbk <minhhuy99dn@gmail.com>
2023-05-26 17:58:28 +0700

    setup backend 1

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+
+
+const PORT = process.env.PORT || 3000
+const app: Application = express()
+
+app.get('/', (req: Request, res:Response) => {
+    res.status(200).send('Express21321321 + Typescript server!!!!')
+})
+
+app.listen(PORT, () => {
+    console.log('server is listening at PORT: ',PORT)
+})
