38c90c52bc3817decf99871e9a012f37f7be672e
huypm <minhhuy99dn@gmail.com>
2023-05-27 13:42:27 +0700

    Feat: finished setup

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,16 +1,35 @@
+/* eslint-disable no-undef */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-    'no-ternary': 'off'
-  }
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'linebreak-style': [
+			'off',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+	}
 }
